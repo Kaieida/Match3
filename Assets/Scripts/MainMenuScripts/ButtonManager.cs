d3f971505ef33813db0a7770f4e9e907fa815d1f
Kaieida <63949971+Kaieida@users.noreply.github.com>
2020-11-30 19:04:32 +0200

    Visual update

---
+++ Assets/Scripts/MainMenuScripts/ButtonManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonManager : MonoBehaviour
+{
+    //[SerializeField] private Sprite secondImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ButtonChange(Sprite sprite)
+    {
+        gameObject.GetComponent<Image>().sprite = sprite;
+    }
+}
